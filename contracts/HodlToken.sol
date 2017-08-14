272018ec7010ba96caf06d7d94e21aa5808adabb
Will Doenlen <wdoenlen@gmail.com>
2017-08-13 22:53:44 -0700

    Working deposit / withdraw

--- contracts/HodlToken.sol
+++ contracts/HodlToken.sol
@@ -2,16 +2,11 @@ pragma solidity ^0.4.11;
 
 import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
-// Create single deposit
-// List out all deposits
-// Withdraw a given deposit or most recent deposit
-// Create hodl tokens 1:1 with eth 
-
 contract HodlToken is StandardToken {
 
     string public name = "Hodl Tokens"; 
     uint8 public decimals = 18;
-    string public symbol = "HODL";         
+    string public symbol = "HODL"; 
     string public version = "H0.1"; 
     
     mapping(address => Deposit[]) public ethDeposits;
@@ -23,6 +18,8 @@ contract HodlToken is StandardToken {
         bool isWithdrawn;
     }
 
+    // We use DepositEvent as a hack to retrieve deposit IDs later on
+    // since we can't return a dynamically-sized list of deposits
     event DepositEvent(
         address indexed depositor,
         bytes32 depositId,
@@ -30,19 +27,21 @@ contract HodlToken is StandardToken {
         uint withdrawDate
     );
 
+    event WithdrawEvent(
+        address indexed withdrawer,
+        bytes32 depositId,
+        uint amount
+    );
+
     function() public payable {
         deposit();
     }
 
-
-    // One deposit per call
-    // No adding to existing deposits
-    // Deposit added to array
     function deposit() public payable returns (bool success) {
         require(msg.value > 0);
         uint withdrawDate = block.timestamp + (1 minutes);
         bytes32 depositId = sha256(uint(msg.sender) + withdrawDate);
-        Deposit memory d = Deposit(depositId, withdrawDate, msg.value, false);
+        Deposit memory d = Deposit(depositId, msg.value, withdrawDate, false);
         ethDeposits[msg.sender].push(d);
         DepositEvent(msg.sender, depositId, msg.value, withdrawDate);
         balances[msg.sender] += msg.value;
@@ -52,18 +51,18 @@ contract HodlToken is StandardToken {
     function withdraw(bytes32 depositId) public returns (bool success) {
         require(balances[msg.sender] > 0);
 
-        Deposit memory d;
         Deposit[] deposits = ethDeposits[msg.sender];
+
         // Using a for loop here is okay since we don't expect users
         // to have too many deposits
         for (var i = 0; i < deposits.length; i++) {
             if (deposits[i].id == depositId) {
-                d = deposits[i];
+                Deposit d = deposits[i];
                 break;
             }
 
-            // If this is true then we didn't find the deposit in the
-            // user's deposits list
+            // If we reach the end of the list then the deposit ID doesn't
+            // exist
             if (i == deposits.length - 1) {
                 return false;
             }
@@ -78,6 +77,7 @@ contract HodlToken is StandardToken {
         d.isWithdrawn = true;
         balances[msg.sender] -= d.amount;
         msg.sender.transfer(d.amount);
+        WithdrawEvent(msg.sender, depositId, d.amount);
         return true;
     }
 
